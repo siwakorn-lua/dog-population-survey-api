dd85238dd4335e0c53019f18dd57ba1871a75c52
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-01-27 05:41:42 +0700

    implement api layout

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var app = express()
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
- 
-app.post('/test', function(req, res) {
-  res.json({notes: "this is only test api"})
-})
- 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-}
 
-)
+app.get("/test", function(req, res) {
+  res.json({ notes: "this is only test api" });
+});
+
+app.post("/register", function(req, res) {
+  // do something here
+});
+
+app.post("/login", function(req, res) {
+  // do something here
+});
+
+app.post("/forgot", function(req, res) {
+  // do something here
+});
+
+app.post("/user/update", function(req, res) {
+  // do something here
+});
+
+app.post("/dog/add", function(req, res) {
+  // do something here
+});
+
+app.post("/dog/update", function(req, res) {
+  // do something here
+});
+
+app.post("/dog/syn", function(req, res) {
+  // do something here
+});
+
+app.post("/report", function(req, res) {
+  // do something here
+});
+
+app.post("/report/request", function(req, res) {
+  // do something here
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
