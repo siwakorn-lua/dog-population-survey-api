df756710d45431f4d8c1e8ea374ea49762807bd3
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-01-27 18:25:12 +0700

    test register

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 var express = require("express");
+var logger = require("morgan");
 var bodyParser = require("body-parser");
 var app = express();
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "doggy",
+  insecureAuth: true
+});
+connection.connect();
 
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/test", function(req, res) {
-  res.json({ notes: "this is only test api" });
-});
-
 app.post("/register", function(req, res) {
-  // do something here
+  let body = req.body;
+  let now = new Date();
+  connection.query("insert into users values ('" +
+  body.username +
+  "','" +
+  body.password +
+  "','" +
+  body.fname +
+  "','" +
+  body.lname +
+  "','" +
+  body.email +
+  "','" +
+  body.profilePicture +
+  "','" +
+  body.address +
+  "','" +
+  now.getFullYear()+"/"+now.getMonth()+"/"+"/"+now.getDate()+" "+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds() +
+  "');", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
 
 app.post("/login", function(req, res) {
